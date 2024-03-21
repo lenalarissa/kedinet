1bbdc56b868dfef428f64d3418b9c99af80e3dae
Lena Heisel <lenaheisel@yahoo.de>
2024-03-21 14:43:02 +0300

    Initialization

---
+++ src/main/java/kedinet/net/KedinetApplication.java
@@ -0,0 +1,13 @@
+package kedinet.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KedinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KedinetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kedinet/net/KedinetApplicationTests.java
@@ -0,0 +1,13 @@
+package kedinet.net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
